65842121966a057493083130c01de3d379e1d249
Quentin DEGRAEVE <qdegraeve@gmail.com>
2017-06-09 19:24:11 +0200

    changed run sh script

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 export DYLD_LIBRARY_PATH=.
 export DYLD_INSERT_LIBRARIES="libft_malloc.so"
 export DYLD_FORCE_FLAT_NAMESPACE=1
-export MALLOC_SHOW_ACTIONS=true
-export MALLOC_SHOW_FREE=true
-export MALLOC_SHOW_MMAP=true
+#export MALLOC_SHOW_ACTIONS=true
+#export MALLOC_SHOW_FREE=true
+#export MALLOC_SHOW_MMAP=true
 $@
 
