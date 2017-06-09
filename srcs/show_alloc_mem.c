dc09ca58c6c89ed7764280f60b5bf2ec7eee1d2a
Quentin DEGRAEVE <qdegraeve@gmail.com>
2017-06-09 17:29:31 +0200

    added show alloc mem

--- srcs/show_alloc_mem.c
+++ srcs/show_alloc_mem.c
@@ -6,7 +6,7 @@
 /*   By: qdegraev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 10:48:20 by qdegraev          #+#    #+#             */
-/*   Updated: 2017/06/09 01:12:04 by qdegraev         ###   ########.fr       */
+/*   Updated: 2017/06/09 17:30:44 by qdegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	print_list(t_meta *list)
 	int		size;
 
 	size = 0;
-	while(list)
+	while (list)
 	{
 		if (!list->free)
 		{
@@ -26,17 +26,24 @@ static int	print_list(t_meta *list)
 			ft_putbase((unsigned long)(list->data + list->size), 16);
 			write(1, " : ", 3);
 			ft_putnbr(list->size);
-			write(1, " octets\n", 8);
+			write(1, " octets", 8);
+			//if ((list->free & debug_show_free()) > 0)
+			//	write(1, " utilisables", 12);
 			size += list->size;
 		}
+//		if (list->heap_start && debug_show_mmap())
+//			ft_putstr("  --   HEAD OF PAGE\n");
+//		else
+			write(1, "\n", 1);
 		list = list->next;
 	}
 	return (size);
 }
 
-void	show_alloc_mem()
+void		show_alloc_mem(void)
 {
 	int		size;
+
 	size = 0;
 	write(1, "TINY : ", 7);
 	ft_putbase((unsigned long)(g_memory.tiny), 16);
