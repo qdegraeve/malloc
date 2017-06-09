1af6a47c999658f2895a50533267296c61413026
Quentin DEGRAEVE <qdegraeve@gmail.com>
2017-06-09 17:00:21 +0200

    added tests

--- tests/test.c
+++ tests/test.c
@@ -1,87 +1,233 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: qdegraev <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/05 10:22:03 by qdegraev          #+#    #+#             */
-/*   Updated: 2017/06/08 17:21:20 by qdegraev         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
-#include "malloc.h"
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-void	show()
+void	show_alloc_mem();
+//void	show_full_mem();
+
+/*void	malloc_calloc(void)
 {
-	write(1, "\n", 1);
+	char	*str;
+	char	*str2;
+	char	*str3;
+	char	*str4;
+	int		i = 0;
+
+	printf("calloc str de 16 octets * 2\n");
+	str = (char*)calloc(2 ,sizeof(char) * 16);
+	while (++i < 16 * 2)
+		printf("%d", str[i]);
+
+	printf("\ncalloc str2 de 1257 octets * 3\n");
+	str2 = (char*)calloc(3, sizeof(char) * 1257);
+	i = 0;
+	while (++i < 3 * 1257)
+		printf("%d", str[i]);
+
+	printf("\ncalloc str3 de 327 octets * 2\n");
+	str3 = (char*)calloc(2, sizeof(char) * 327);
+	i = 0;
+	while (++i < 2 * 327)
+		printf("%d", str[i]);
+
+	printf("\ncalloc str4 de 42 octets * 5\n");
+	str4 = (char*)calloc(5, sizeof(char) * 42);
+	i = 0;
+	while (++i < 5 * 42)
+		printf("%d", str[i]);
+	printf("\n");
+
+	printf("Allocated memory:\n");
 	show_alloc_mem();
-	write(1, "\n", 1);
-}
+	printf("\nFull process memory\n");
+	show_full_mem();
+}*/
 
-void	text(char *str)
+void	malloc_malloc(void)
 {
-	printf("\b\b--  %s  --\n", str);
+	char	*str;
+	char	*str2;
+	char	*str3;
+	char	*str4;
+	char	*str5;
+	char	*str6;
+	char	*str7;
+	char	*str8;
+
+	printf("malloc str de 16 octets\n");
+	str = (char*)malloc(sizeof(char) * 16);
+	printf("malloc str2 de 1257 octets\n");
+	str2 = (char*)malloc(sizeof(char) * 1257);
+	printf("malloc str3 de 327 octets\n");
+	str3 = (char*)malloc(sizeof(char) * 327);
+	printf("malloc str4 de 42 octets\n");
+	str4 = (char*)malloc(sizeof(char) * 42);
+	printf("malloc str5 de 1 octet\n");
+	str5 = (char*)malloc(sizeof(char) * 1);
+	printf("malloc str6 de 4055 octet\n");
+	str6 = (char*)malloc(sizeof(char) * 4055);
+	printf("malloc str7 de 0 octet\n");
+	str7 = (char*)malloc(sizeof(char) * 0);
+	printf("malloc str8 de -2 octet\n\n");
+	str8 = (char*)malloc(sizeof(char) * -2);
+
+	int i = 0;
+	char *tab[1024];
+	while(i < 1024)
+	{
+		tab[i] = malloc(24);
+		i++;
+	}
+	i= 0;
+	while(i < 1024)
+	{
+		free(tab[i]);
+		i++;
+	}
+	printf("Allocated memory:\n");
+	show_alloc_mem();
+	printf("\nFull process memory\n");
 }
 
-int main() {
-	char *ptr = NULL;
-	char *ptr2 = NULL;
-	char *ptr3 = NULL;
-	char *ptr4 = NULL;
-	char *ptr5 = NULL;
-	char *ptr6 = NULL;
-	char *ptr8 = NULL;
-	char ptr9[10];
-	text("allocation de 6 pointeurs de taille 10 50 100 100 100 250 et 5000");
-	text("la zones TINY doit contenir 5 allocation et la zone LARGE doit en contenir une");
-	ptr = malloc(10);
-	ptr = strcpy(ptr, "caca pipi\0");
-	ptr2 = malloc(50);
-	ptr2 = strcpy(ptr2, "jesuistresbeau\0");
-	ptr3 = malloc(100);
-	ptr3 = strcpy(ptr3, "et aussi tres intelligent\0");
-	ptr4 = malloc(100);
-	ptr5 = malloc(250);
-	ptr6 = malloc(5000);
-	show();
-	text("appel a free() sur les pointeurs 3 2 puis 4");
-	free(ptr3);
-	free(ptr2);
-	free(ptr4);
-	show();
-	text("allocation d'un pointeur de 240 octets");
-	text("celui ci doit s'inserer apres le premier pointeur de 10 octets et avant ceuil de 250");
-	ptr2 = malloc(200);
-	show();
-	text("reallocation du pointeur 5 de 250 a 255 octets");
-	text("son addresse doit rester identique");
-	ptr5 = realloc(ptr5, 255);
-	show();
-	text("reallocation du pointeur 2 de 200 a 250 octets");
-	text("son adresse doit rester identique");
-	text("reallocation du pointeur 1 de 10 a 50 octets");
-	text("son address doit etre differente mais son contenu identique");
-	text("realloc d'un pointeur null sur ptr8 de 500 octets (zone SMALL)");
-	ptr2 = realloc(ptr2, 250);
-	printf("ptr before realloc == [%s]\n", ptr);
-	ptr = realloc(ptr, 50);
-	printf("ptr after realloc == [%s]\n", ptr);
-	ptr8 = realloc(NULL, 500);
-	show();
-	text("appel a free sur le poiteur 8 de 500 octets");
-	text("la zone SMALL doit a nouveau avoir une adresse a 0X0");
-	free(ptr8);
-	show();
-	text("appel a free sur tous les pointeurs restant");
-	text("toutes les zones doivent avoir une adress a 0x0");
-	free(ptr);
-	free(ptr2);
-	free(ptr5);
-	free(ptr6);
-	show();
-	text("appel a free sur un pointeur non malloc, le programme doit abort avec un message d'erreur");
-	free(ptr9);
+void	malloc_realloc(void)
+{
+	char	*str;
+	char	*str2;
+	char	*str3;
+	char	*str4;
+
+	printf("malloc str de 16 octets\n");
+	str = (char*)malloc(sizeof(char) * 16);
+	printf("malloc str2 de 1257 octets\n");
+	str2 = (char*)malloc(sizeof(char) * 1257);
+	printf("malloc str3 de 327 octets\n");
+	str3 = (char*)malloc(sizeof(char) * 327);
+	printf("malloc str4 de 42 octets\n\n");
+	str4 = (char*)malloc(sizeof(char) * 42);
+
+	printf("Allocated memory:\n");
+	show_alloc_mem();
+
+	printf("\nrealloc str de 27 octets\n");
+	str = (char *)realloc(str, sizeof(char) * 27);
+	printf("realloc str3 de 233 octets\n");
+	str3 = (char *)realloc(str3, sizeof(char) * 233);
+	printf("realloc str2 de 1337 octets\n");
+	str2 = (char *)realloc(str2, sizeof(char) * 1337);
+	printf("realloc str4 de 2100 octets\n\n");
+	str4 = (char *)realloc(str4, sizeof(char) * 2100);
+
+	printf("Allocated memory:\n");
+	show_alloc_mem();
+	printf("\nFull process memory\n");
+}
+
+void	malloc_free(void)
+{
+	char	*str;
+	char	*str2;
+	char	*str3;
+	char	*str4;
+
+	printf("malloc str de 16 octets\n");
+	str = (char*)malloc(sizeof(char) * 16);
+	printf("malloc str2 de 1257 octets\n");
+	str2 = (char*)malloc(sizeof(char) * 1257);
+	printf("malloc str3 de 327 octets\n");
+	str3 = (char*)malloc(sizeof(char) * 327);
+	printf("malloc str4 de 42 octets\n\n");
+	str4 = (char*)malloc(sizeof(char) * 42);
+
+	printf("Allocated memory:\n");
+	show_alloc_mem();
+	printf("\nFull process memory\n");
+
+	printf("\nfree str\n");
+	free(str);
+	printf("Allocated memory:\n");
+	show_alloc_mem();
+
+	printf("\nfree str2\n");
+	free(str2);
+	printf("Allocated memory:\n");
+	show_alloc_mem();
+
+	printf("\nfree str3\n");
+	free(str3);
+	printf("Allocated memory:\n");
+	show_alloc_mem();
+
+	printf("\nfree str4\n");
+	free(str4);
+	printf("Allocated memory:\n");
+	show_alloc_mem();
+	printf("Full process memory\n");
+
+	printf("\nfree str4\n");
+	free(str4);
+}
+
+void	u_stupid_malloc_test(void)
+{
+	int		i;
+	void	*allocation;
+
+	i = 0;
+	srand(time(NULL));
+	printf("--- Stupid test begin ---\n");
+	printf("--- Random stuff: ---\n");
+	while (i < 1000)
+	{
+		int uji = rand() % 2000;
+		allocation = (void *)malloc(sizeof(char) * uji);
+		free(allocation);
+		i++;
+	}
+	printf("--- Fat alloc ---\n");
+	allocation = NULL;
+	allocation = (void *)malloc(sizeof(char) * 65535);
+	i = 5;
+	while (i > 0)
+	{
+	printf("--- Fat realloc ---\n");
+		allocation = (void *)realloc(allocation, 65535 / i);	
+		printf("\n");
+		i--;
+	}
+	free(allocation);
+}
+
+int		main(int ac, char **av)
+{
+
+	(void)ac;
+	if (av[1] && !(av[1][1]))
+	{
+		if (av[1][0] == '1')
+		{
+			printf("malloc_MALLOC\n");
+			malloc_malloc();
+		}
+		else if (av[1][0] == '2')
+		{
+			printf("malloc_REALLOC\n");
+			malloc_realloc();
+		}
+		else if (av[1][0] == '3')
+		{
+			printf("malloc_FREE\n");
+			malloc_free();
+		}
+		else if (av[1][0] == '4')
+		{
+			printf("malloc_STUPID\n");
+			u_stupid_malloc_test();
+		}
+
+		else
+			printf("please choose what malloc to do :\n1 -> malloc\n2 -> realloc\n3 -> free\n4 -> stupid malloc\n");
+	}
+	else
+		printf("please choose what malloc to do :\n1 -> malloc\n2 -> realloc\n3 -> free\n4 -> stupid malloc\n");
+	return (0);
 }
